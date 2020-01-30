83686cb2ad2cacae1395fd7e42014ef18cfee0ae
hyde <rmlee0003@gmail.com>
2020-01-29 21:04:36 -0500

    building out server file

---
+++ api/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const helmet = require('helmet')
+const userRouter = require('../user/user-router')
+const server = express()
+server.use(helmet())
+server.use(express.json())
+
+server.use('/api/user',userRouter)
