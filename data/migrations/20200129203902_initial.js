fb7abf892260a181f7351a3b1b6b58dae1f65937
hyde <rmlee0003@gmail.com>
2020-01-29 21:42:19 -0500

    knex schema user and trips

--- data/migrations/20200129203902_initial.js
+++ data/migrations/20200129203902_initial.js
@@ -1,8 +1,28 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable("users", (user) => {
+      user.increments("id")
+      user.string("name",255).notNullable()
+      user.string("username",255).notNullable()
+      user.boolean("org").defaultTo(false)
+      user.string("password",255).notNullable()
+      user.string("bio")
+      user.string("email")
+  })
+  await knex.schema.createTable("experience", (exp) => {
+    exp.increments("id")
+    exp.string("location").notNullable()
+    exp.string("description").notNullable()
+    exp.float("lat")
+    exp.float("long")
+    exp.boolean("private").defaultTo(false)
+    exp.string("type").notNullable()
+    exp.string("imgurl")
+    exp.string("duration").notNullable()
+
+})
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('users')
 };
